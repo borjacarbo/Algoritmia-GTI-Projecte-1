b1523946d0ada8f2791cfddf7fee878ee3fff203
Borja Carbó <Borja.Carbo.UPV@gmail.com>
2019-02-20 21:09:40 +0100

    Vsn 1.0.0
    Classe per a prova de la funcionalitat de la classe Disco

---
+++ TestDisco.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class TestDisco
+    {
+        static TestDisco()
+        {
+
+            /*---------------------------------------------*/
+            /*  TESTING:
+                Creació de discos */
+            /*---------------------------------------------*/
+            Console.WriteLine(" =================================================================================================");
+            Console.WriteLine("  Creació de discos ");
+            Disco disco_1 = new Disco();
+            disco_1.ValorInt = 1;
+            int Valor_1 = disco_1.ValorInt;
+
+            Disco disco_2 = new Disco();
+            disco_2.ValorInt = 2;
+            int Valor_2 = disco_2.ValorInt;
+
+            Disco disco_3 = new Disco();
+            disco_3.ValorInt = 3;
+            int Valor_3 = disco_3.ValorInt;
+
+            Console.WriteLine("Disco 1 Creat amb valor: " + Valor_1.ToString());
+            Console.WriteLine("Disco 2 Creat amb valor: " + Valor_2.ToString());
+            Console.WriteLine("Disco 3 Creat amb valor: " + Valor_3.ToString());
+            Console.WriteLine();
+
+        }
+    }
+}
