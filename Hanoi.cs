47da05f6042f6d1ea30d8a9acce130f2e08e9c2d
Borja Carbó <Borja.Carbo.UPV@gmail.com>
2019-02-21 08:24:11 +0100

    Vsn 1.0.0
    Introducció de la funcionalitat mover_disco/2

--- Hanoi.cs
+++ Hanoi.cs
@@ -9,9 +9,34 @@ namespace Torres_de_Hanoi
     class Hanoi
     {
         /*TODO: Implementar métodos*/
-        public void mover_disco(Pila a, Pila b)
+        public void mover_disco(ref Pila a, ref Pila b)
         {
-
+            if ((b.Top == 0) || (a.Top == 0))
+            {
+                if ((b.Top == 0))
+                {
+                    Disco disco = a.pop();
+                    b.push(disco);
+                }
+                if ((a.Top == 0))
+                {
+                    Disco disco = b.pop();
+                    a.push(disco);
+                }
+            }
+            else
+            {
+                if (a.Top > b.Top)
+                {
+                    Disco disco = b.pop();
+                    a.push(disco);
+                }
+                if (a.Top < b.Top)
+                {
+                    Disco disco = a.pop();
+                    b.push(disco);
+                }
+            }
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
